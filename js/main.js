1f12c9a7f2bb5596d543e8b060a219c216e5006e
Ryan Gonzales <helloryantg@gmail.com>
2018-09-19 09:24:24 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Blackjack Card Game
+
+/* 
+Goal of the game - beat the dealer by getting a count as close to 21
+
+One = 1 or 11
+Face Cards = 10
+
+functions:
+function initGame() {
+    // Shuffle Cards
+    // Render board
+    // Deal cards
+    // Check if 21
+        // If (player === 21) && (dealer !== 21)
+            // player wins
+            // game over
+        // else if (player !== 21) && (dealer === 21) 
+            // dealer wins
+            // game over
+}
+
+function render() {
+    // Render Score
+    // Render Gameboard
+    // If there is a winner
+        // Render Winning Message
+    // else 
+        // Render Turn Message
+    // Deal 1 card per player face up
+    // Deal 1 card facedown
+    // Alternate distribution
+}
+
+function gameOver() {
+    // 
+}
+
+
+
+*/
+
+
+/*----- constants -----*/
+const deckTotal = 52;
+
+/*----- app's state (variables) -----*/
+var player;
+var dealer;
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
