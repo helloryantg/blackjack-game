8f744808b268a037b4d08e927e99f146639e3d2a
Ryan Gonzales <helloryantg@gmail.com>
2018-09-20 12:15:15 -0700

    replace readme.md

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# blackjack-game
+# Blackjack Card Game
+
+* Goal of the game - beat the dealer by getting a count as close to 21
+
+## User stores:
+1) User must be able to press start button.
+2) User must be able to select to hold, hit, or double buttons.
+3) User must be able to input dollar amount of money.
+4) User must be able to reset the game (start game button turns into reset once game has rendered).
+5) User must be able to quit game to go back to pop-up screen.
+
+## Gameplay
+* Player gets 2 cards face up
+* Dealer gets 1 card face up and 1 card face down
+* Player gets to input dollar value for bets 
+* If Player cards equal 21 
+    * Dealer flips cards
+        * Dealer conditions
+    * If Dealer also has 21
+        * It's a tie 
+    * If not, Player wins.
+    * Shuffle cards
+* While Player cards is less than 21
+    * Player gets to choose to hold, hit, or double (if matching cards)
+    * Check for cards equal 21
+        * Keep in mind that Ace cards count as 1 or 11
+* If Player cards is greater than 21 
+    * Player busts, Dealer wins
+    * Shuffle cards
+
+### Conditions
+* If total >= 18 stay
+* If total <= 18 hit
+* If total === 21 blackjack
+* If no bust - compare values with player
+* If dealer > player - dealer wins
+* If dealer < player - player wins
+* If dealer === player - it's a tie
+
+
+#### Technologies
+* HTML 
+* CSS
+* jQuery
+
+#### Game
+* Array of card objects for main deck
+    * Card values = 1 - 10 (10 for all face cards)
+    * Suits (unecessary?)
+* Player and Dealer - seperate Arrays to compare to each other which is greater
+* Nested for-loops for the suits? 
+* Use CSS-framework for cards
+
+
+##### Notes
+* Make sure to add state - when cards are in play - don't allow betting/holding/staying
+* Z-index for stacking
+
+##### Variables
+
+
+functions:
+function initGame() {
+    // Shuffle Cards
+    // Render board
+    // Deal cards
+    // Check if 21
+        // If (player === 21) && (dealer !== 21)
+            // player wins
+            // player get round chips
+            // start next round
+        // else if (player !== 21) && (dealer === 21) 
+            // dealer wins
+            // dealer gets round chips
+            // start next round
+        // else if (player === 21) && ((dealer === 21)
+            // it's a tie
+            // player and dealer get chips back
+}
+
+function render() {
+    // Render Score
+    // Render Gameboard
+    // If there is a winner
+        // Render Winning Message
+    // else 
+        // Render Turn Message
+    // Deal 1 card per player face up
+    // Deal 1 card facedown
+    // Alternate distribution
+}
+
+function gameOver() {
+    // 
+}
+*/
--- js/main.js
+++ js/main.js
@@ -1,87 +1,96 @@
-// Blackjack Card Game
-
-/* 
-Goal of the game - beat the dealer by getting a count as close to 21
-
-User stores:
-1) User must be able to press start button
-2) User must be able to select to stand button or hit button
-3) User must be able to bet 1, 5, 10, or 20 amount of money - all buttons
-4) User must be able to stay or hit (or split - BONUS)
-5) User must be able to reset the game
-
-// Player goes first
-
-// Turn 1 - player
-// Each player gets 2 cards face up
-// Dealer gets 1 card face up and 1 card face down
-// Player gets 1 card face up and 1 card face down
-// Player gets to bet 1, 5, 10, or 20 chips
-// Player card show
-// If not 21 or under 21, draw cards until 17 or under. (check if this is the right one - maybe add probability?)
-    // If over 21, check if ace is considered 1 or 11
-// Players get to choose to stand or hit
-// If stand - push total and change turn
-// If hit add another random card to the array
-// check if the current total is 21
-    // If 21, BUST! dealer wins
-    // If not, prompt user to stay or hit
-    // If stay - push total to array and change turn
-// Dealer turn
-// If total >= 18 stay
-// If total <= 18, hit - check if 21
-// if no bust - compare values
-// If dealer > player - dealer wins
-    // If dealer < player - player wins
-    // If dealer > player - dealer wins
-// Game ends
-
-
-
-functions:
-function initGame() {
-    // Shuffle Cards
-    // Render board
-    // Deal cards
-    // Check if 21
-        // If (player === 21) && (dealer !== 21)
-            // player wins
-            // player get round chips
-            // start next round
-        // else if (player !== 21) && (dealer === 21) 
-            // dealer wins
-            // dealer gets round chips
-            // start next round
-        // else if (player === 21) && ((dealer === 21)
-            // it's a tie
-            // player and dealer get chips back
-}
-
-function render() {
-    // Render Score
-    // Render Gameboard
-    // If there is a winner
-        // Render Winning Message
-    // else 
-        // Render Turn Message
-    // Deal 1 card per player face up
-    // Deal 1 card facedown
-    // Alternate distribution
-}
-
-function gameOver() {
-    // 
-}
-
-
-
-*/
 
 
 /*----- constants -----*/
-const deckTotal = 52;
+const deck = [
+    {dA: [1, 11]},
+    {hA: [1, 11]},
+    {sA: [1, 11]},
+    {cA: [1, 11]},
+    {dK: 10},
+    {hK: 10},
+    {sK: 10},
+    {cK: 10},
+    {dQ: 10},
+    {hQ: 10},
+    {sQ: 10},
+    {cQ: 10},
+    {dJ: 10},
+    {hJ: 10},
+    {sJ: 10},
+    {cJ: 10},
+    {d10: 10},
+    {h10: 10},
+    {s10: 10},
+    {c10: 10},
+    {d09: 9},
+    {h09: 9},
+    {s09: 9},
+    {c09: 9},
+    {d08: 8},
+    {h08: 8},
+    {s08: 8},
+    {c08: 8},
+    {d07: 7},
+    {h07: 7},
+    {s07: 7},
+    {c07: 7},
+    {d06: 6},
+    {h06: 6},
+    {s06: 6},
+    {c06: 6},
+    {d05: 5},
+    {h05: 5},
+    {s05: 5},
+    {c05: 5},
+    {d04: 4},
+    {h04: 4},
+    {s04: 4},
+    {c04: 4},
+    {d03: 3},
+    {h03: 3},
+    {s03: 3},
+    {c03: 3},
+    {d02: 2},
+    {h02: 2},
+    {s02: 2},
+    {c02: 2}
+];
+
+var player = {
+    credit: 1000,
+    cardsValue: 0,
+    hit: function() {
+
+    },
+    stay: function() {
+
+    },
+    double: function() {
+
+    },
+    winCredit: function(amount) {
+        this.credit += amount;
+    },
+    loseCredit: function(amount) {
+        this.credit -= amount;
+    }
+}
+
+var dealer = {
+    cardsValue: 0,
+    hit: function() {
+
+    },
+    stay: function() {
+
+    }
+}
 
 /*----- app's state (variables) -----*/
+var state; // State of the game
+
+var dealerHand; // Array for dealer
+var playerHand; // Array for player
 
 var player;
 var playerCash;
@@ -91,21 +100,7 @@ var turn;
 var score;
 var round;
 
-var state = {
-    ace: [1, 11],
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-    ten: 10,
-    jack: 10,
-    queen: 10,
-    king: 10,
-}
+
 
 
 /*----- cached element references -----*/
